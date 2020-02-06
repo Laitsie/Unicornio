dd52d86d8d77addb735d23e2b6e364235da4888e
Rosie <Rosie@rosies-macbook-pro.local>
2020-02-06 17:21:10 -0300

    Initial Commit

---
+++ Unicornio/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Unicornio
+//
+//  Created by Rosie on 2/6/20.
+//  Copyright © 2020 Rosie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
